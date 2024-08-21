c41aa9b33b516948b01cbc834d2d2ded8fd5bb6b
fijicist <anuranjansarkar4@gmail.com>
2024-08-21 21:03:53 +0530

    added harpoon to nvim config

--- nvim/.config/nvim/lua/fijicist/plugins/formatting.lua
+++ nvim/.config/nvim/lua/fijicist/plugins/formatting.lua
@@ -20,6 +20,7 @@ return {
         liquid = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
+        neorg = { "prettier" },
       },
       format_on_save = {
         lsp_fallback = true,
